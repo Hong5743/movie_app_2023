5a11e2558f34718da3d84695a76c81fa9a72145d
Hong5743 <ghd5743@gmail.com>
2023-10-17 10:43:09 +0900

    react 2일차 컴포넌트 사용 및 map()함수 사용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Movie({ name, picture }) {
+  // { fav } = props;
+  return (
+  <div>
+    <h2>I love { name } movie</h2>
+    <img src={picture}/>
+  </div>
+  )
+}
+
+const movieLike = [
+  {
+    id: 1,
+    name: 'horror',
+    image: 'https://file.mk.co.kr/meet/neds/2023/08/image_readtop_2023_642055_16927752025596352.jpg',
+  },
+  {
+    id: 2,
+    name: 'action',
+    image: 'https://mblogthumb-phinf.pstatic.net/20160928_265/kkk10aaa10_1475072214030e2br2_JPEG/%BF%F8%C3%DF1-1.jpg?type=w800',
+  },
+  {
+    id: 3,
+    name: 'comedy',
+    image: 'https://image.tving.com/upload/cms/caim/CAIM2100/M000369853.jpeg/dims/resize/F_webp,480',
+  }
+];
+
+function renderMovie(list) {
+  return <Movie key={list.id} name={list.name} picture={list.image}/>;
+}
 
 function App() {
+  console.log(movieLike.map(renderMovie));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {movieLike.map(renderMovie)}
+      {/* <h1>Hello</h1> */}
+      {/* <Movie fav="horror" />
+      <Movie fav="action" />
+      <Movie fav="comedy" /> */}
     </div>
   );
 }
